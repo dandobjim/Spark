d9bef8ea4ff6b5d93a4b543653405c7da80a2b2b
ddoblas <dandobjim@gmail.com>
2025-08-06 12:59:10 +0200

    Add machine learning pipeline for customer purchase analysis using KMeans

--- spark-the-definitive-guide/ch3/ch3.py
+++ spark-the-definitive-guide/ch3/ch3.py
@@ -1,7 +1,11 @@
 import os
 import glob
+
+from pyspark.ml import Pipeline
+from pyspark.ml.clustering import KMeans
+from pyspark.ml.feature import StringIndexer, OneHotEncoder, VectorAssembler
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import window, column, desc, col
+from pyspark.sql.functions import window, column, desc, col, date_format
 
 
 spark = SparkSession.builder \
@@ -36,6 +40,8 @@ staticDataFrame\
 
 
 ### STREAMING
+"""# Uncomment the following lines to run the streaming example
+# Note: This requires a Spark environment with streaming capabilities.
 streamingDataFrame = spark.readStream \
     .schema(staticSchema) \
     .option("maxFilesPerTrigger", 1) \
@@ -64,12 +70,55 @@ purchaseByCustomerPerHour.writeStream \
     .outputMode("complete") \
     .start()
 
-spark.sql("""
+spark.sql(
           SELECT *
           FROM customer_purchases
           ORDER BY `sum(total_cost)` DESC
-          """) \
+          ) \
     .show(5)
-
+"""
 
 ### ML and Analytics
+"""
+preppedDataFrame = staticDataFrame \
+    .na.fill(0)\
+    .withColumn("day_of_week",
+                date_format(col("InvoiceDate"), "EEEE")) \
+    .coalesce(5)
+
+trainDataFrame = preppedDataFrame \
+    .where("InvoiceDate < '2011-07-01'")
+testDataFrame = preppedDataFrame \
+    .where("InvoiceDate >= '2011-07-01'")
+
+print("Training DataFrame Count: ", trainDataFrame.count())
+print("Test DataFrame Count: ", testDataFrame.count())
+
+print("========== INDEXER ===========")
+indexer = StringIndexer(inputCol="day_of_week", outputCol="day_of_week_index")
+print("========== ENCODER ===========")
+encoder = OneHotEncoder(inputCol="day_of_week_index", outputCol="day_of_week_encoded")
+print("========== Assembler ===========")
+vectorAssembler = VectorAssembler(inputCols=["day_of_week_encoded", "UnitPrice", "Quantity"],
+                                  outputCol="features")
+print("========== PIPELINE ===========")
+transformationPipeline = Pipeline(stages=[indexer, encoder, vectorAssembler])
+
+print("========== FIT ===========")
+fittedPipeline = transformationPipeline.fit(trainDataFrame)
+
+print("========== TRANSFORM ===========")
+transformedTraining = fittedPipeline.transform(trainDataFrame)
+transformedTraining.cache()
+
+print("========== TRAIN ===========")
+kmeans = KMeans()\
+    .setK(20)\
+    .setSeed(1)
+
+
+kmModel = kmeans.fit(transformedTraining)
+transformedTest = fittedPipeline.transform(testDataFrame)
+"""
+
+# LOWER LEVEL API
