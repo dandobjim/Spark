4fbbe8ddc86830faa7d3f38c76596f8fea231469
ddoblas <dandobjim@gmail.com>
2025-08-07 15:36:43 +0200

    Add DataFrame transformations and operations in ch5.py for enhanced data analysis

--- spark-the-definitive-guide/ch5/ch5.py
+++ spark-the-definitive-guide/ch5/ch5.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import col, lit, expr
 from pyspark.sql.types import StructField, StructType, StringType, LongType
 
 spark = SparkSession.builder.master("local[*]").getOrCreate()
@@ -70,3 +70,92 @@ df.selectExpr(
     "AVG(count)",
     "COUNT(DISTINCT(DEST_COUNTRY_NAME))"
 ).show(2)
+
+"""
+SELECT *, 1 as One
+FROM dfTable LIMIT 2
+"""
+df.select(expr("*"),
+          lit(1).alias("One")).show(2)
+
+df.withColumn("numberOne", lit(1)).show(2)
+
+######## ADD COLUMNS
+
+df.withColumn("withinCountry", expr("ORIGIN_COUNTRY_NAME = DEST_COUNTRY_NAME")).show(2)
+
+print(df.withColumnRenamed("DEST_COUNTRY_NAME", "dest").columns)
+
+######## Special Characters
+
+dfWithColName = df.withColumn("This Long Column-Name", expr("ORIGIN_COUNTRY_NAME"))
+dfWithColName.selectExpr("`This Long Column-Name`",
+                         "`This Long Column-Name` AS `new col`").show(2)
+
+##### DROP
+
+print(df.drop("ORIGIN_COUNTRY_NAME").columns)
+print(dfWithColName.drop("ORIGIN_COUNTRY_NAME", "DEST_COUNTRY_NAME").columns)
+
+###### CAST
+"""
+SELECT *, CAST(count AS long) AS count2
+FROM dfTable
+"""
+df.withColumn("count2", col("count").cast(LongType())).printSchema()
+
+#### FILTERING
+"""
+SELECT *
+FROM dfTable
+WHERE count < 2 LIMIT 2
+"""
+df.filter(col("count") < 2).show(2)
+df.where("count < 2").show(2)
+
+"""
+SELECT *
+FROM dfTable
+WHERE count < 2
+  AND ORIGIN_COUNTRY_NAME != 'Croatia'
+LIMIT 2
+"""
+
+df.filter(col("count") < 2) \
+    .filter(col("ORIGIN_COUNTRY_NAME") != 'Croatia') \
+    .show(2)
+
+df.where("count < 2") \
+    .where("ORIGIN_COUNTRY_NAME != 'Croatia'") \
+    .show(2)
+
+df.where(col("count") < 2) \
+    .where(col("ORIGIN_COUNTRY_NAME") != "Croatia") \
+    .show(2)
+
+###### UNIQUE
+"""
+SELECT COUNT(DISTINCT (ORIGIN_COUNTRY_NAME, DEST_COUNTRY_NAME))
+FROM dfTable
+"""
+print(df.select("ORIGIN_COUNTRY_NAME", "DEST_COUNTRY_NAME").distinct().count())
+
+"""
+SELECT COUNT DISTINCT ORIGIN_COUNTRY_NAME
+FROM dfTable
+"""
+
+print(df.select("ORIGIN_COUNTRY_NAME").distinct().count())
+
+####### RANDOM SAMPLES
+
+seed = 5
+withReplacement = False
+fraction = 0.5
+print(df.sample(withReplacement, fraction, seed).count())
+
+#### RANDOM SPLITS
+dataFrames = df.randomSplit([0.25, 0.75], seed=seed)
+print(dataFrames[0].count() > dataFrames[1].count())
+
+#### UNION
